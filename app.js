5c0cd85c98d37f355f9355824bfb84991a8d970d
Alessandro Benedetti <alessandro@aditsolutions.it>
2015-04-07 19:19:11 +0200

    -Neo4j with node API

---
+++ app.js
@@ -0,0 +1,67 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var expressCors = require('express-cors')
+var logger = require('morgan');
+var http = require('http');
+
+var search_manager = require('./search')
+
+var app = express()
+
+app.use(logger('dev'))
+
+var port = process.env.NODE_ENV.toLowerCase() == "production"? process.env.PORT : 3000;
+
+app.set('port',port)
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json())
+app.use(expressCors({
+	allowedOrigins: ['http://localhost:*','http://*.xdiscovery.com:*']
+}))
+
+app.listen(app.get('port'), function () {
+  	console.log('Discovery-API Server listening on port ' + app.get('port'));
+});
+
+app.get('/api/v1/node/search/', function (req,res) {
+	search_manager.search_node({
+		name:'Tavolino Pr'
+	},function (data) {
+		res.json(data)
+	})
+})
+
+app.get('/api/v1/node/:identifier', function (req,res) {
+	search_manager.get_node(parseInt(req.params.identifier),function(data) {
+		res.json(data)
+	})
+})
+
+app.post('/api/v1/node/:identifier/user', function (req,res) {
+
+	search_manager.user_metrics(_node,_user,_type,completionHandler,function(data){
+		res.json(data)
+	})
+})
+
+app.post('/api/v1/node/:identifier/neighbors', function (req,res) {
+
+	var _limit=25
+	var _offset=0
+
+	if (req.body.limit!==undefined && req.body.limit!="") 
+		_limit = parseInt(req.body.limit)
+
+	if (req.body.offset!==undefined && req.body.offset!="") 
+		_offset = parseInt(req.body.offset)
+
+	search_manager.get_node_neighbors(parseInt(req.params.identifier),_limit,_offset,function(data) {
+		res.json(data)
+	})
+})
+
+app.get('*', function(req,res) {
+	res.send("");
+}); 
---
+++ search.js
@@ -0,0 +1,102 @@
+var es = require("elasticsearch")
+var neo4j = require('neo4j');
+
+graph = new neo4j.GraphDatabase('http://localhost:7474');
+client = new es.Client;
+
+exports.search_node = function (terms,completionHandler)  {
+
+	client.search({
+	  	index: 'jesproduct',
+	  	type: 'string',
+	  	body: {
+	    	query: {
+	    		match:terms
+	    	}
+	  	}
+	}).then(function (resp) {
+    	var hits = resp;
+    	
+    	if ((completionHandler !== undefined) && (typeof(completionHandler) == "function"))
+			completionHandler({success:true,data:hits})
+
+	}, function (err) {
+    	console.trace(err.message);
+
+    	if ((completionHandler !== undefined) && (typeof(completionHandler) == "function"))
+			completionHandler({success:false,errorCode:"Error while getting data from ElasticSearch"})    	
+	});
+
+}
+
+exports.user_metrics = function(_node,_user,_type,completionHandler) {
+	//type can be START,END(,HOP)
+
+	graph.cypher({
+		query: 'MATCH (product:Product) WHERE product.identifier = {identifier} CREATE UNIQUE (user:User {identifier:{u_identifier}})-[:'+_type.toUpperCase()+']->(product) RETURN user',
+		params: {
+			identifier: _node,
+			u_identifier: _user,
+			type: _type
+		}
+	}, function (err, results) {
+	    if (err)
+	    {
+	    	if ((completionHandler !== undefined) && (typeof(completionHandler) == "function"))
+				completionHandler({success:false,errorCode:"Error while getting data from Neo4j"})
+		}
+		else
+		{
+		    if ((completionHandler !== undefined) && (typeof(completionHandler) == "function"))
+				completionHandler({success:true,data:results})
+		}
+	});
+
+}
+
+exports.get_node = function (node,completionHandler) {
+
+	graph.cypher({
+	    query: 'MATCH (el:Product) WHERE el.identifier = {identifier} RETURN el',
+	    params: {
+	        identifier: node
+	    }
+	}, function (err, results) {
+	    if (err)
+	    {
+	    	if ((completionHandler !== undefined) && (typeof(completionHandler) == "function"))
+				completionHandler({success:false,errorCode:"Error while getting data from Neo4j"})
+		}
+		else
+		{
+		    if ((completionHandler !== undefined) && (typeof(completionHandler) == "function"))
+				completionHandler({success:true,data:results})
+		}
+	});
+
+}
+
+exports.get_node_neighbors = function (node,_limit,_offset,completionHandler) {
+
+	graph.cypher({
+	    query: 'MATCH (single:Product)-[:CORRELATO]-(product) WHERE single.identifier = {identifier} RETURN DISTINCT product SKIP {offset} LIMIT {limit}',
+	    params: {
+	        identifier: node,
+	        limit: _limit,
+	        offset: _offset	        
+	    }
+	}, function (err, results) {
+	    if (err)
+	    {
+	    	console.log(err)
+	    	if ((completionHandler !== undefined) && (typeof(completionHandler) == "function"))
+				completionHandler({success:false,errorCode:"Error while getting data from Neo4j"})
+		}
+		else
+		{
+		    if ((completionHandler !== undefined) && (typeof(completionHandler) == "function"))
+				completionHandler({success:true,data:results})
+		}
+	});
+
+}
